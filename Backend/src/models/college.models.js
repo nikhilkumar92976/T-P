57815cfe4fdb0fa85ff142b5605090a2e463af89
Mr_Adrs <adrshmishra018@gmail.com>
2025-04-30 21:54:46 +0530

    basic setup complete , admin register and login functionality complete

--- Backend/src/models/college.models.js
+++ Backend/src/models/college.models.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
-
-const collegeSchema = new mongoose.Schema({
-  name: {
+import bcrypt from "bcrypt"
+const adminSchema = new mongoose.Schema({
+  fullName: {
     type: String,
-    required: true,
+    // required: true,
     index: true,
     trim: true,
   },
@@ -28,16 +28,52 @@ const collegeSchema = new mongoose.Schema({
   },
   phone: {
     type:String,
-    required:true,
+    // required:true,
     trim:true
   },
   college_id: {
     type:String,
-    required
+    // required:true
   },
   subject:[{
     type:String,
   }],
+  refreshToken:{
+    type:String,
+  }
 },{timestamps:true});
 
-export const College = mongoose.model("College", collegeSchema);
+adminSchema.pre("save", async function (next){
+  // if not modified 
+  if(!this.isModified("password"))return next();
+
+  // if ask to modified
+  this.password = await bcrypt.hash(this.password,10)
+  next()
+});
+
+adminSchema.methods.isPasswordCorrect = async (requestPassword) => {
+  return await bcrypt.compare(requestPassword, this.password);
+};
+adminSchema.methods.generateAccessToken = function() {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      email: this.email,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+adminSchema.methods.generateRefreshToken = function() {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
+export const Admin = mongoose.model("Admin", adminSchema);
--- Backend/src/models/student.models.js
+++ Backend/src/models/student.models.js
@@ -1,47 +1,110 @@
 import mongoose from "mongoose";
+const studentSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: [true, "Full name is required"],
+      trim: true,
+      index: true,
+    },
+    username: {
+      type: String,
+      required: [true, "username is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        "Please provide a valid email address",
+      ],
+      index: true,
+    },
+    phone: {
+      type: String,
+      required: [true, "Phone number is required"],
+      trim: true,
+      match: [/^\d{10}$/, "Phone number must be 10 digits"],
+      index: true,
+    },
+    enrollment: {
+      type: String,
+      required: [true, "Enrollment number is required"],
+      match: [/\d{4}[A-Z]{2}\d{6}$/, "Enrollment number is required"],
+      unique: true,
+      trim: true,
+    },
+    branch: {
+      type: String,
+      default: "N/A",
+    },
+    batch: {
+      type: Date,
+      default: null,
+    },
+    photo: {
+      type: String,
+      to,
+    },
+    total_tests_appeared: {
+      type: Number,
+      default: 0,
+    },
+    average_score: {
+      type: Number,
+      default: 0,
+    },
+    last_test_date: {
+      type: Date,
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
 
-const studentSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    index: true,
-    trim: true,
-  },
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-    index: true,
-  },
-  password: { 
-    type: String,
-    required: [true, "password is required"] 
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-  phone: {
-    type:String,
-    required:true,
-    trim:true
-  },
-  rollno: String,
-  branch: String,
-  batch: Date,
-  photo: String,
-  total_tests_appeared: {
-    type: Number,
-    default: 0,
-  },
-  average_score: {
-    type: Number,
-    default: 0,
-  },
-  last_test_date: Date,
-},{timestamps:true});
+studentSchema.pre("save", async function (next){
+  // if not modified 
+  if(!this.isModified("password"))return next();
+
+  // if ask to modified
+  this.password = await bcrypt.hash(this.password,10)
+  next()
+});
+
+studentSchema.methods.isPasswordCorrect = async (requestPassword) => {
+  return await bcrypt.compare(requestPassword, this.password);
+};
+studentSchema.methods.generateAccessToken = (this) => {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      email: this.email,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+studentSchema.methods.generateRefreshToken = (this) => {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
+
+export const Student = mongoose.model("Student", studentSchema);
